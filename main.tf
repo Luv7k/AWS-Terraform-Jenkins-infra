6c21c946029015a4be82b7c434350a0ddcb71d91
Lovesh Bishnoi <bishno01@ads.uni-passau.de>
2024-03-12 23:35:07 +0100

    LB, HZ and CM added

--- main.tf
+++ main.tf
@@ -36,15 +36,15 @@ module "lb_target_group" {
   vpc_id                   = module.networking.dev_proj_2_vpc_id
   ec2_instance_id          = module.ec2.dev_proj_2_ec2_instance_id
 }
-/*
+
 module "alb" {
   source                    = "./load-balancer"
-  lb_name                   = "dev-proj-1-alb"
+  lb_name                   = "dev-proj-2-alb"
   is_external               = false
   lb_type                   = "application"
   sg_enable_ssh_https       = module.security_group.sg_ec2_sg_ssh_http_id
-  subnet_ids                = tolist(module.networking.dev_proj_1_public_subnets)
-  tag_name                  = "dev-proj-1-alb"
+  subnet_ids                = tolist(module.networking.dev_proj_2_public_subnets)
+  tag_name                  = "dev-proj-2-alb"
   lb_target_group_arn       = module.lb_target_group.dev_proj_2_lb_target_group_arn
   ec2_instance_id           = module.ec2.dev_proj_2_ec2_instance_id
   lb_listner_port           = 5000
@@ -52,7 +52,7 @@ module "alb" {
   lb_listner_default_action = "forward"
   lb_https_listner_port     = 443
   lb_https_listner_protocol = "HTTPS"
-  dev_proj_1_acm_arn        = module.aws_ceritification_manager.dev_proj_1_acm_arn
+  dev_proj_2_acm_arn        = module.aws_ceritification_manager.dev_proj_2_acm_arn
   lb_target_group_attachment_port = 5000
 }
 
@@ -68,11 +68,11 @@ module "aws_ceritification_manager" {
   domain_name    = var.domain_name
   hosted_zone_id = module.hosted_zone.hosted_zone_id
 }
-
+/*
 module "rds_db_instance" {
   source               = "./rds"
   db_subnet_group_name = "dev_proj_1_rds_subnet_group"
-  subnet_groups        = tolist(module.networking.dev_proj_1_public_subnets)
+  subnet_groups        = tolist(module.networking.dev_proj_2_public_subnets)
   rds_mysql_sg_id      = module.security_group.rds_mysql_sg_id
   mysql_db_identifier  = "mydb"
   mysql_username       = "dbuser"
